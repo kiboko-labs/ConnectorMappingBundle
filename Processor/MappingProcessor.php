e0ab9dcf81d1930e87fd34470dca964380cbcad1
sumbobyboys <julien@akeneo.com>
2014-03-26 11:57:55 +0100

    added fixtures processor

--- Processor/MappingProcessor.php
+++ Processor/MappingProcessor.php
@@ -15,7 +15,7 @@ use Akeneo\Bundle\BatchBundle\Step\StepExecutionAwareInterface;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class FamilyMappingProcessor extends AbstractConfigurableStepElement implements
+class MappingProcessor extends AbstractConfigurableStepElement implements
     ItemProcessorInterface,
     StepExecutionAwareInterface
 {
